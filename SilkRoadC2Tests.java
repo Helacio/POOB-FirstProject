07e17411f6cda4d19e1c6b9631e2f1e18413b219
HDavid Sánchez <hsanchezalarcon@gmail.com>
2025-10-13 19:27:50 -0500

    Corrigiendo errores

--- SilkRoadC2Tests.java
+++ SilkRoadC2Tests.java
@@ -1,6 +1,4 @@
 import static org.junit.Assert.*;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -14,35 +12,40 @@ public class SilkRoadC2Tests
     @Test
     public void shouldCreateSilkRoadEmpty(){
         SilkRoad silkRoad = new SilkRoad(10);
-        assertTrue(silkRoad.getOk());
+        silkRoad.makeInvisible();
+        assertTrue("SilkRoad should be created succesfully", silkRoad.getOk());
     }
     @Test
     public void shouldAddRobot(){
         SilkRoad silkRoad = new SilkRoad(15);
         silkRoad.addRobot(5);
+        silkRoad.makeInvisible();
         
-        assertTrue(silkRoad.getOk());
+        assertTrue("Robot should be added succesfully", silkRoad.getOk());
     }
     @Test
     public void shouldAddShop(){
         SilkRoad silkRoad = new SilkRoad(15);
         silkRoad.addShop(8, 25);
+        silkRoad.makeInvisible();
         
-        assertTrue(silkRoad.getOk());
+        assertTrue("Shop should be added succesfully", silkRoad.getOk());
     }
     @Test
     public void shoudFailWithTwoRobotsInSamePosition(){
         SilkRoad silkRoad = new SilkRoad(15);
         silkRoad.addRobot(5);
         silkRoad.addRobot(5);
-        assertFalse(silkRoad.getOk());
+        silkRoad.makeInvisible();
+        assertFalse("Should fail when two robots are in the same position", silkRoad.getOk());
     }
     @Test
     public void shoudFailWithTwoShopsInSamePosition(){
         SilkRoad silkRoad = new SilkRoad(15);
         silkRoad.addShop(8, 25);
         silkRoad.addShop(8, 25);
-        assertFalse(silkRoad.getOk());
+        silkRoad.makeInvisible();
+        assertFalse("Should fail whe two shops are in the same position", silkRoad.getOk());
     }
     @Test
     public void shouldMoveRobot(){
@@ -50,7 +53,8 @@ public class SilkRoadC2Tests
         SilkRoad silkRoad = new SilkRoad(input);
         
         silkRoad.moveRobot(10, 15);
-        assertTrue(silkRoad.getOk());
+        silkRoad.makeInvisible();
+        assertTrue("Robot should move succesfully", silkRoad.getOk());
     }
     @Test
     public void ShouldFailWhenMoveRobotFails() {
@@ -58,19 +62,21 @@ public class SilkRoadC2Tests
         SilkRoad silkRoad = new SilkRoad(input);
         
         silkRoad.moveRobot(99, 15);
-        assertFalse(silkRoad.getOk());
+        silkRoad.makeInvisible();
+        assertFalse("Should fail when trying to move a non exisstent robot", silkRoad.getOk());
     }
     @Test
     public void shouldReturnProfitPermove(){
         int[] input = {1, 10, 2, 15 ,25};
         SilkRoad silkRoad = new SilkRoad(input);
+        silkRoad.makeInvisible();
         
         Robot r = silkRoad.getRobots().get(10);
         
         silkRoad.moveRobot(10, 15);
         int profit = silkRoad.profitPerMove(10, 0);
         
-        assertEquals(20, profit);
+        assertEquals("Profit per move should be calculated correctly", 20, profit);
     }
     @Test
     public void shouldSortRobotsAscendingByLocation(){
@@ -78,12 +84,13 @@ public class SilkRoadC2Tests
         silkRoad.addRobot(5);
         silkRoad.addRobot(2);
         silkRoad.addRobot(10);
+        silkRoad.makeInvisible();
         
         int[][] sorted = silkRoad.robots();
         
-        assertEquals(2, sorted[0][0]);
-        assertEquals(5, sorted[1][0]);
-        assertEquals(10, sorted[2][0]);
+        assertEquals("First robot should have the lowest position", 2, sorted[0][0]);
+        assertEquals("Second robot should have the middle position", 5, sorted[1][0]);
+        assertEquals("Third robot should have the highest position",10, sorted[2][0]);
     }
     @Test
     public void shouldSortShopsAscendingByLocation(){
@@ -91,11 +98,12 @@ public class SilkRoadC2Tests
         silkRoad.addShop(5, 30);
         silkRoad.addShop(10, 35);
         silkRoad.addShop(19, 27);
+        silkRoad.makeInvisible();
         
         int[][] sorted = silkRoad.stores();
         
-        assertEquals(5, sorted[0][0]);
-        assertEquals(10, sorted[1][0]);
-        assertEquals(19, sorted[2][0]);
+        assertEquals("First shop should have the lowest position", 5, sorted[0][0]);
+        assertEquals("Second shop should have the middle position", 10, sorted[1][0]);
+        assertEquals("Third shop should have the highest position", 19, sorted[2][0]);
     }
 }
